33736ea8fec782394e4a6ccd36af4a71e11a77c8
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-12 19:44:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+window.onload = function() {
+    
+    const targetElement = document.getElementById("level");
+    
+   
+    if (!targetElement) {
+        alert("Element with id 'level' not found");
+        return;
+    }
+    
+   
+    let level = 0;
+    
+    
+    let currentElement = targetElement;
+
+	while (currentElement) {
+
+		level++;
+
+		currentElement = currentElement.parentElement;
+	}
+
+	alert("The level of the element is: " + level);
+
+}
+
+
+
+
+	
